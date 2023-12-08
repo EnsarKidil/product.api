652bcc42f387379ad4d3d394db65ba7eeef5a072
EnsarKidil <129101925+EnsarKidil@users.noreply.github.com>
2023-12-08 04:01:51 +0300

    Added an .env file to the project, it does not do much but it contains the database and port informations. Also corrected some mistakes in swagger documentation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //This is the app file
 //App file is the place that our API will run
+require('dotenv').config();
+
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./Swagger/swagger'); // Swagger konfigürasyonu dosyanız
 const express = require('express');
@@ -9,14 +11,18 @@ const { errorHandler } = require('./CustomMiddlewares/ErrorHandlingMiddleware');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger/swagger.yml');
 const app = express();
-const port = 3000;
+// dotenv ile çevresel değişkenlere erişim
+const port = process.env.PORT
+console.log(port);
+const mongoURI = process.env.MONGODB_URI;
+console.log(mongoURI);
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use('/Product', productRoutes);
 app.use(errorHandler);
 
-mongoose.connect('mongodb://localhost:27017/ProductsDB') //MongoDB connection
+mongoose.connect(mongoURI) //MongoDB connection
     .then(() => {console.log("Connection Successful!")}) //If successful
     .catch(err => console.log(err)); //If there is an error
 
